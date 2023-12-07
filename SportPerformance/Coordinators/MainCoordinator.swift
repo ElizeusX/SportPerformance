aa995e7500c6f072be3ac135997da20141370db0
Elizeus <elizeus.chrabrov@applifting.cz>
2023-12-07 16:40:56 +0100

    Delete main storyboard and connect MainCoordinator

---
+++ SportPerformance/Coordinators/MainCoordinator.swift
@@ -0,0 +1,23 @@
+//
+//  MainCoordinator.swift
+//  SportPerformance
+//
+//  Created by Elizeus Chrabrov on 07.12.2023.
+//
+
+import SwiftUI
+
+class MainCoordinator: UINavigationController {
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        self.setViewControllers(
+            [UIHostingController(rootView: PerformanceListView())],
+            animated: false
+        )
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ SportPerformance/PerformanceListScreen/PerformanceListView.swift
@@ -0,0 +1,18 @@
+//
+//  PerformanceListView.swift
+//  SportPerformance
+//
+//  Created by Elizeus Chrabrov on 07.12.2023.
+//
+
+import SwiftUI
+
+struct PerformanceListView: View {
+    var body: some View {
+        Text("Sport Performance")
+    }
+}
+
+#Preview {
+    PerformanceListView()
+}
