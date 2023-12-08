be1af61d1fe450054d638f7bb49196baa3dcc0aa
Elizeus <elizeus.chrabrov@applifting.cz>
2023-12-08 20:24:14 +0100

    Add constraints

---
+++ SportPerformance/Constraints.swift
@@ -0,0 +1,15 @@
+//
+//  Constraints.swift
+//  SportPerformance
+//
+//  Created by Elizeus Chrabrov on 08.12.2023.
+//
+
+import Foundation
+
+enum Constraints {
+    enum Padding {
+        static let small: CGFloat = 8
+        static let standard: CGFloat = 16
+    }
+}
