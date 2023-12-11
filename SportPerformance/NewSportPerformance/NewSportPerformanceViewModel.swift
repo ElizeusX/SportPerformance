80330f0515b217566027756bb9a2f17e2247f13d
Elizeus <elizeus.chrabrov@applifting.cz>
2023-12-11 13:08:03 +0100

    Add NewSportPerformanceViewModel

---
+++ SportPerformance/NewSportPerformance/NewSportPerformanceViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  NewSportPerformanceViewModel.swift
+//  SportPerformance
+//
+//  Created by Elizeus Chrabrov on 10.12.2023.
+//
+
+import Foundation
+
+class NewSportPerformanceViewModel: ObservableObject {
+
+    @Published var hours = 0
+    @Published var minutes = 0
+    @Published var seconds = 0
+}
