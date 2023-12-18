9ec4b158b65f27bfe1558ea4eab48869dce94e52
Elizeus <elizeus.chrabrov@applifting.cz>
2023-12-18 10:40:25 +0100

    Add tests for filter

--- SportPerformanceTests/SportPerformanceTests.swift
+++ SportPerformanceTests/SportPerformanceTests.swift
@@ -6,9 +6,12 @@
 //
 
 import XCTest
+import Combine
 @testable import SportPerformance
 
 final class SportPerformanceTests: XCTestCase {
+
+    var subscriptions: Set<AnyCancellable> = []
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -33,4 +36,75 @@ final class SportPerformanceTests: XCTestCase {
         }
     }
 
+    override func tearDown() {
+        super.tearDown()
+        subscriptions.removeAll()
+    }
+
+    func testCreateSportPerformanceListWithSuccess() {
+        let expectation = XCTestExpectation(description: "Filtered data should be loaded successfully")
+        let viewModel = SportPerformanceListViewModel(
+            coordinator: nil,
+            firebaseStoreManager: MockFirebaseStoreManager(),
+            dataPersistenceManager: MockDataPersistenceManager()
+        )
+        let sortedMockData = MockData.performanceData.sorted(by: { $0.date > $1.date })
+        viewModel.selectedRepository = .all
+        viewModel.$filteredPerformanceCollection
+            .dropFirst()
+            .sink { data in
+                XCTAssertEqual(data, sortedMockData)
+                expectation.fulfill()
+            }
+            .store(in: &subscriptions)
+
+        wait(for: [expectation], timeout: 1)
+        XCTAssertNil(viewModel.alertConfig)
+    }
+
+    func testFilterWithLocal() {
+        let expectation = XCTestExpectation(description: "Filtered data should be loaded successfully")
+        let viewModel = SportPerformanceListViewModel(
+            coordinator: nil,
+            firebaseStoreManager: MockFirebaseStoreManager(),
+            dataPersistenceManager: MockDataPersistenceManager()
+        )
+        let localSortedMockData = MockData.performanceData
+            .sorted(by: { $0.date > $1.date })
+            .filter { $0.repository == .local}
+        viewModel.selectedRepository = .local
+        viewModel.$filteredPerformanceCollection
+            .dropFirst()
+            .sink { data in
+                XCTAssertEqual(data, localSortedMockData)
+                expectation.fulfill()
+            }
+            .store(in: &subscriptions)
+
+        wait(for: [expectation], timeout: 1)
+        XCTAssertNil(viewModel.alertConfig)
+    }
+
+    func testFilterWithRemote() {
+        let expectation = XCTestExpectation(description: "Filtered data should be loaded successfully")
+        let viewModel = SportPerformanceListViewModel(
+            coordinator: nil,
+            firebaseStoreManager: MockFirebaseStoreManager(),
+            dataPersistenceManager: MockDataPersistenceManager()
+        )
+        let localSortedMockData = MockData.performanceData
+            .sorted(by: { $0.date > $1.date })
+            .filter { $0.repository == .remote}
+        viewModel.selectedRepository = .remote
+        viewModel.$filteredPerformanceCollection
+            .dropFirst()
+            .sink { data in
+                XCTAssertEqual(data, localSortedMockData)
+                expectation.fulfill()
+            }
+            .store(in: &subscriptions)
+
+        wait(for: [expectation], timeout: 1)
+        XCTAssertNil(viewModel.alertConfig)
+    }
 }
